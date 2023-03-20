78802eafa31b7bd8e34100df9c931a186454abaa
babailanxx@gmail.com <babailanxx@gmail.com>
2023-03-20 19:17:48 +0800

    Add project files.

---
+++ SirChysler/MainWindow.xaml.vb
@@ -0,0 +1,14 @@
+﻿Class MainWindow
+    Private Sub Submit_button(sender As Object, e As RoutedEventArgs)
+        Dim NameGreet, AgeGreet, AddressGreet As String
+        NameGreet = "Hello I am" & Name.Text
+        AgeGreet = "I am" & Age.Text
+        AddressGreet = "I lived in" & Address.Text
+        MsgBox(NameGreet & Chr(10) & AgeGreet & Chr(10) & AddressGreet)
+    End Sub
+    Private Sub Clear(sender As Object, e As RoutedEventArgs)
+        Name.Text = ""
+        Age.Text = ""
+        Address.Text = ""
+    End Sub
+End Class
